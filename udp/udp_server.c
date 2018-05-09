5275f27da2b74de8ad4a781b98e5548a45f68b51
mrwicks <myfakeemail@mailinator.com>
2018-05-09 11:44:31 -0700

    Using getnameinfo to get the source address in udp_server.

--- udp/udp_server.c
+++ udp/udp_server.c
@@ -4,6 +4,10 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+#include <sys/socket.h>
+#include <netinet/ip.h> 
+#include <netdb.h>
+
 int main (int argc, char **argv)
 {
   int iPort = 4000;
@@ -38,16 +42,43 @@ int main (int argc, char **argv)
   {
     char buffer[200];
     int iLength;
+    struct sockaddr src_addr;
+    socklen_t addrlen;
+    struct sockaddr_in *in4 = (struct sockaddr_in *)&src_addr;
+    struct sockaddr_in6 *in6 = (struct sockaddr_in6 *)&src_addr;
+    char hostname [NI_MAXHOST] = "";
+    int iError = 0;
 
-    iLength = recvfrom (iFd, buffer, sizeof (buffer)-1, 0, NULL, 0);
-    if ( iLength < 0 )
+    addrlen = sizeof (src_addr);
+    iLength = recvfrom (iFd, buffer, sizeof (buffer)-1, 0, &src_addr, &addrlen);
+    if (iLength < 0)
     {
       perror ("recvfrom");
       break;
     }
 
     buffer[iLength] = '\0';
-    printf("%d bytes: '%s'\n", iLength, buffer);
+    printf("%d bytes: '%s': ", iLength, buffer);
+
+    
+    iError = getnameinfo (&src_addr, addrlen, hostname, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+    if (iError != 0)
+    {
+      fprintf(stderr, "error in getnameinfo: %s\n", gai_strerror(iError));
+      hostname[0] = '\0';
+    }
+
+    switch (src_addr.sa_family)
+    {
+    case AF_INET:
+      printf ("IPV4, port %d, IP %s\n", in4->sin_port, hostname);
+      break;
+    case AF_INET6:
+      printf ("IPV6, port %d, IP %s\n", in6->sin6_port, hostname);
+      break;
+    default:
+      printf ("What family is this?\n");
+    }
   }
 
   close (iFd);
