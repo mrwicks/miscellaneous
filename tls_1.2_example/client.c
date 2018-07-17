e6a0044ab6326ad7ddaea14d16b2087232a5c96d
Mr. Wicks <rich_wicks@yahoo.com>
2018-07-16 17:56:03 -0700

    Allowing this to be laid out flat.

--- tls_1.2_example/client.c
+++ tls_1.2_example/client.c
@@ -22,8 +22,6 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-#define FAIL    -1
-
 #define LOCAL_ABORT()                              \
 do                                                 \
 {                                                  \
@@ -31,14 +29,16 @@ do                                                 \
   abort ();                                        \
 } while (0)
 
+#define USE_FUNCTIONS 0
 
+#if (USE_FUNCTIONS)
 int OpenConnection (const char *hostname, uint16_t port)
 {
   int sd;
   struct hostent *host;
   struct sockaddr_in addr;
 
-  if ( (host = gethostbyname (hostname)) == NULL )
+  if ((host = gethostbyname (hostname)) == NULL)
   {
     perror (hostname);
     LOCAL_ABORT ();
@@ -49,7 +49,7 @@ int OpenConnection (const char *hostname, uint16_t port)
   addr.sin_port = htons (port);
   addr.sin_addr.s_addr = * (long*) (host->h_addr);
 
-  if ( connect (sd, (struct sockaddr*)&addr, sizeof (addr)) != 0 )
+  if (connect (sd, (struct sockaddr*)&addr, sizeof (addr)) != 0)
   {
     close (sd);
     perror (hostname);
@@ -68,7 +68,7 @@ SSL_CTX* InitCTX (void)
   SSL_load_error_strings ();         /* Bring in and register error messages */
   method = TLSv1_2_client_method (); /* Create new client-method instance */
   ctx = SSL_CTX_new (method);        /* Create new context */
-  if ( ctx == NULL )
+  if (ctx == NULL)
   {
     ERR_print_errors_fp (stderr);
     LOCAL_ABORT ();
@@ -81,7 +81,7 @@ void ShowCerts (SSL* ssl)
   X509 *cert;
   char *line;
   cert = SSL_get_peer_certificate (ssl); /* get the server's certificate */
-  if ( cert != NULL )
+  if (cert != NULL)
   {
     printf ("Server certificates:\n");
     line = X509_NAME_oneline (X509_get_subject_name (cert), 0, 0);
@@ -97,6 +97,7 @@ void ShowCerts (SSL* ssl)
     printf ("Info: No client certificates configured.\n");
   }
 }
+#endif
 
 int main (int argc, char **argv)
 {
@@ -107,8 +108,13 @@ int main (int argc, char **argv)
   int bytes;
   char *hostname;
   uint16_t portnum;
+#if (!(USE_FUNCTIONS))
+  struct hostent *host;
+  struct sockaddr_in addr;
+  const SSL_METHOD *method;
+#endif
 
-  if ( argc != 3 )
+  if (argc != 3)
   {
     printf ("usage: %s <hostname> <portnum>\n", argv[0]);
     exit (0);
@@ -120,16 +126,52 @@ int main (int argc, char **argv)
   hostname = argv[1];
   portnum = atoi (argv[2]);
 
+#if (USE_FUNCTIONS)
   ctx = InitCTX ();
   server = OpenConnection (hostname, portnum);
+#else
+  OpenSSL_add_all_algorithms ();     /* Load cryptos, et.al. */
+  SSL_load_error_strings ();         /* Bring in and register error messages */
+  method = TLSv1_2_client_method (); /* Create new client-method instance */
+  ctx = SSL_CTX_new (method);        /* Create new context */
+  if (ctx == NULL)
+  {
+    ERR_print_errors_fp (stderr);
+    LOCAL_ABORT ();
+  }
+
+  if ((host = gethostbyname (hostname)) == NULL)
+  {
+    perror (hostname);
+    LOCAL_ABORT ();
+  }
+  server = socket (PF_INET, SOCK_STREAM, 0);
+  bzero (&addr, sizeof (addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons (portnum);
+  addr.sin_addr.s_addr = * (long*) (host->h_addr);
+
+  if (connect (server, (struct sockaddr*)&addr, sizeof (addr)) != 0)
+  {
+    close (server);
+    perror (hostname);
+    fprintf (stderr, "Is the server running, and on the correct port (%d)?\n", portnum);
+    LOCAL_ABORT ();
+  }
+#endif
+
   ssl = SSL_new (ctx);      /* create new SSL connection state */
   SSL_set_fd (ssl, server);    /* attach the socket descriptor */
-  if ( SSL_connect (ssl) == FAIL )   /* perform the connection */
+  if (SSL_connect (ssl) <= 0)   /* perform the connection */
   {
     ERR_print_errors_fp (stderr);
   }
   else
   {
+#if (!(USE_FUNCTIONS))
+    X509 *cert;
+    char *line;
+#endif
     char szRequest[4096];
     sprintf (szRequest, 
              "GET / HTTP/1.1\r\n"
@@ -143,7 +185,28 @@ int main (int argc, char **argv)
     printf ("Sending:\n[%s]\n", szRequest);
 
     printf ("\n\nConnected with %s encryption\n", SSL_get_cipher (ssl));
+
+#if (USE_FUNCTIONS)
     ShowCerts (ssl);        /* get any certs */
+#else
+    cert = SSL_get_peer_certificate (ssl); /* get the server's certificate */
+    if (cert != NULL)
+    {
+      printf ("Server certificates:\n");
+      line = X509_NAME_oneline (X509_get_subject_name (cert), 0, 0);
+      printf ("Subject: %s\n", line);
+      free (line);       /* free the malloc'ed string */
+      line = X509_NAME_oneline (X509_get_issuer_name (cert), 0, 0);
+      printf ("Issuer: %s\n\n", line);
+      free (line);       /* free the malloc'ed string */
+      X509_free (cert);  /* free the malloc'ed certificate copy */
+    }
+    else
+    {
+      printf ("Info: No client certificates configured.\n");
+    }
+#endif
+
     SSL_write (ssl, szRequest, strlen (szRequest));   /* encrypt & send message */
 
     bytes = SSL_read (ssl, buf, sizeof (buf)); /* get reply & decrypt */
